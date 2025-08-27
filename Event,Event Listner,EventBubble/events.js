efe3930bd55b713a140a9285ee2e6bc82c3735e1
Rimpa Kasundi <rimpakasundi@gmail.com>
2025-08-27 20:12:32 +0530

    first commit

---
+++ Event,Event Listner,EventBubble/events.js
@@ -0,0 +1,11 @@
+console.log("external file");
+
+// option 2 to handle event
+function makeYellow() {
+  document.body.style.backgroundColor = "yellow";
+}
+function makeRed() {
+  document.body.style.backgroundColor = "red";
+}
+
+// option 3: get Element by Id and and then set onclick
